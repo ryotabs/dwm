0373cfa529cb5836281392621cd8c0d927d3ff71
Ryota Seki <ryotabs@hawaii.edu>
2020-05-14 23:52:38 -1000

    Remove monocle layout

--- config.h
+++ config.h
@@ -41,7 +41,6 @@ static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "[]=",      tile },    /* first entry is default */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
-	{ "[M]",      monocle },
 	{ "(@)",      spiral },
 	{ "[//]",     dwindle },
 };
